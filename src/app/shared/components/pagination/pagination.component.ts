6237568bfe0cb75e57acbe8dfdcc2daf66eea8a7
Joachim Pihlgren <j.pihlgren@outlook.com>
2024-10-13 16:00:54 +0200

    Handle TMDB weirdness in the TMDB class

--- src/app/shared/components/pagination/pagination.component.ts
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -8,34 +8,31 @@ import { Pagination } from '../../models/interfaces/pagination';
     templateUrl: './pagination.component.html',
     styleUrl: './pagination.component.css'
 })
-export class PaginationComponent implements  OnInit {
+export class PaginationComponent implements OnInit {
 
     pagination: InputSignal<Pagination> = input.required<Pagination>()
     pageRequest: OutputEmitterRef<number> = output<number>()
-    pageButtons!: Signal<PageButton[]> 
+    pageButtons!: Signal<PageButton[]>
 
     generatePageButtons(pagination: Pagination): PageButton[] {
-        // The tmdb response property total_page is often 999,
-        // but calling 999 gives 400 response saying 500 is max...
-        const TMDB_MAX_PAGE = 500
         const pageButtons: PageButton[] = []
         const page: number = pagination.page
-        const maxPage: number = Math.min(pagination.totalPages - 1, TMDB_MAX_PAGE)
-        pageButtons.push({page: 1, isCurrent: false, disabled: page === 1 , content: `<<`})
-        if(page >= 2) pageButtons.push({page: page - 1, isCurrent: false, disabled: page < 2, content: `${page - 1}`})
-        pageButtons.push({page: page, isCurrent: true, disabled: true, content: `${page}`})
-        if(page < maxPage) pageButtons.push({page: page + 1, isCurrent: false, disabled: page >= maxPage , content: `${page + 1}`})
-        pageButtons.push({page: maxPage, isCurrent: false, disabled: page === maxPage, content: `>>`})
+        const maxPage: number = pagination.totalPages - 1
+        pageButtons.push({ page: 1, isCurrent: false, disabled: page === 1, content: `<<` })
+        if (page >= 2) pageButtons.push({ page: page - 1, isCurrent: false, disabled: page < 2, content: `${page - 1}` })
+        pageButtons.push({ page: page, isCurrent: true, disabled: true, content: `${page}` })
+        if (page < maxPage) pageButtons.push({ page: page + 1, isCurrent: false, disabled: page >= maxPage, content: `${page + 1}` })
+        pageButtons.push({ page: maxPage, isCurrent: false, disabled: page === maxPage, content: `>>` })
         return pageButtons
     }
 
     requestPage(page: number): void {
-       this.pageRequest.emit(page)
+        this.pageRequest.emit(page)
     }
 
     ngOnInit(): void {
         console.log(this.pagination())
-        this.pageButtons = computed(() => this.generatePageButtons(this.pagination())) 
+        this.pageButtons = computed(() => this.generatePageButtons(this.pagination()))
     }
 }
 
