64e3908a5e71bbac851400e4d2eb67dade419cbf
Joachim Pihlgren <j.pihlgren@outlook.com>
2025-04-21 00:19:34 +0200

    refactor styling

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,14 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { ButtonComponent } from '../../button/button.component';
 import { RoutingService } from '../../services/routing.service';
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [RouterLink, RouterLinkActive, ButtonComponent],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+    selector: 'app-header',
+    standalone: true,
+    imports: [RouterLink, RouterLinkActive],
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css'
 })
 export class HeaderComponent {
     protected routingService: RoutingService = inject(RoutingService)
