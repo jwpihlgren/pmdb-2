2f96a817662b6b64d36fda6a1d04a949a6bc706a
Joachim Pihlgren <j.pihlgren@outlook.com>
2025-11-25 23:16:25 +0100

    update the seasons component with the new simple list

--- src/app/pages/detailed-show/components/seasons/seasons.component.ts
+++ src/app/pages/detailed-show/components/seasons/seasons.component.ts
@@ -1,17 +1,34 @@
 import { Component, inject, Signal } from '@angular/core';
 import { ImageComponent, ImageParams } from '../../../../shared/components/image/image.component';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { DetailedShow } from '../../../../shared/models/interfaces/detailed-show';
 import { map } from 'rxjs';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { Location } from '@angular/common';
 import { ConfigService } from '../../../../shared/services/config.service';
+import { SimpleListPageComponent, ListItemComponent, SimpleListPageOptions } from
+    "../../../../shared/pages/simple-list-page/simple-list-page.component";
 
 @Component({
-  selector: 'app-seasons',
-  imports: [RouterLink, ImageComponent],
-  templateUrl: './seasons.component.html',
-  styleUrl: './seasons.component.css'
+    selector: 'app-seasons',
+    imports: [ImageComponent, ListItemComponent, SimpleListPageComponent],
+    template: `
+<app-simple-list-page [back]="['../..']" [options]="listParams" [loaded]="!!detailedShow()">
+    @for(season of detailedShow().seasons; track $index) {
+    <app-list-item [link]="['./', season.seasonNumber.toString()]">
+        <ng-container slot="image">
+            <app-image [params]=createImageParams(season)></app-image>
+        </ng-container>
+        <ng-container slot="title">
+            {{season.name}} ({{season.episodeCount}} episodes)
+        </ng-container>
+        <ng-container slot="content">
+            {{season.overview}}
+        </ng-container>
+    </app-list-item>
+    }
+</app-simple-list-page>
+`,
 })
 export class SeasonsComponent {
     protected activatedRoute: ActivatedRoute = inject(ActivatedRoute)
@@ -24,7 +41,7 @@ export class SeasonsComponent {
             map(data => {
                 return data["show"] as DetailedShow
             })
-        ), {requireSync: true})
+        ), { requireSync: true })
     }
 
 
@@ -36,8 +53,12 @@ export class SeasonsComponent {
 
     createImageParams(season: DetailedShow["seasons"][0]): ImageParams {
         return {
-            aspectRatio: {denominator: 3, numerator: 2},
+            aspectRatio: { denominator: 3, numerator: 2 },
             src: season.posterImagePath,
         }
+    }
+
+    listParams: SimpleListPageOptions = {
+        title: "Seasons"
     }
 }
