b48005bd278d765d68696905abb5ed177782ef4e
Joachim Pihlgren <j.pihlgren@outlook.com>
2025-11-27 13:20:22 +0100

    begin style new grid

---
+++ src/app/shared/components/simple-grid/simple-grid.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-grid',
+  imports: [],
+  templateUrl: './simple-grid.component.html',
+  styleUrl: './simple-grid.component.css'
+})
+export class SimpleGridComponent {
+
+}
