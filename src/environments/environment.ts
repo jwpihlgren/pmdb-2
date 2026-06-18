9a28afba854d49ad827af75d43c137e719c21b8c
Joachim Pihlgren <j.pihlgren@outlook.com>
2026-06-18 23:10:12 +0200

    add environment files becuase theyre no secret anyway

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    tmdbApiKey: "2bdd872281cf9ceeb45dd62b79fbdf06",
+    tmdbApiUrl: "https://api.themoviedb.org/3/",
+    tmdbProxyUrl: "https://tmdb-file-proxy.pages.dev/api/keywords",
+    storageKeyPrefix: "pmdb-2",
+    imdbMediaUrl: "https://www.imdb.com/title/",
+    imdbPeopleUrl: "https://www.imdb.com/name/",
+    dummyImageApiUrl: "https://dummyimage.com",
+    isoCountriesApiUrl: "https://restcountries.com/v3.1/all?fields=cca2,name"
+};
